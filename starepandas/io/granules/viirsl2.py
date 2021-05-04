03d72237cfa4c4d80d927537d729003fde967986
niklasphabian <griessbaum@gmail.com>
2021-05-04 13:00:47 -0700

    sync

--- starepandas/io/granules/viirsl2.py
+++ starepandas/io/granules/viirsl2.py
@@ -3,10 +3,10 @@ import starepandas.io.s3
 import numpy
 
 
-class VIIRS_L2(Granule):
+class VIIRSL2(Granule):
     
     def __init__(self, file_path, sidecar_path=None):
-        super(VIIRS_L2, self).__init__(file_path, sidecar_path)
+        super(VIIRSL2, self).__init__(file_path, sidecar_path)
         self.nom_res = '750m'
         self.netcdf = starepandas.io.s3.nc4_Dataset_wrapper(self.file_path, 'r', format='NETCDF4')
     
@@ -19,19 +19,19 @@ class VIIRS_L2(Granule):
         self.lon = self.netcdf.groups['geolocation_data']['longitude'][:].data.astype(numpy.double)
            
 
-class CLDMSK_L2_VIIRS(VIIRS_L2):
+class CLDMSKL2VIIRS(VIIRSL2):
     
     def __init__(self, file_path, sidecar_path=None):
-        super(CLDMSK_L2_VIIRS, self).__init__(file_path, sidecar_path)        
+        super(CLDMSKL2VIIRS, self).__init__(file_path, sidecar_path)
         
     def read_data(self):
-        '''
-        reads the data from a CLDMSK_L2_VIIRS granule into the self.data dictionary.
+        """
+        reads the data from a CLDMSKL2VIIRS granule into the self.data dictionary.
         Only the Integer_Cloud_Mask is read. The values are to be interpreted as:
         0 = cloudy, 1 = probably cloudy, 2 = probably clear, 3 = confident clear, -1 = no result
-                
+
         :return: None
-        '''
+        """
 
         self.data['Integer_Cloud_Mask'] = self.netcdf.groups['geophysical_data']['Integer_Cloud_Mask'][:].data
 
@@ -40,37 +40,52 @@ class CLDMSK_L2_VIIRS(VIIRS_L2):
         # self.data['Quality_Assurance'] = self.netcdf.groups['geophysical_data']['Quality_Assurance'][:].data
 
 
-        
-
-class VNP03MOD(VIIRS_L2):
+class VNP03MOD(VIIRSL2):
     
     def __init__(self, file_path, sidecar_path=None):
         super().__init__(file_path, sidecar_path)        
         
-    def read_data(self):        
-        # 1: Shallow_Ocean 2: Land 3: Coastline 4: Shallow_Inland 5: Ephemeral 6: Deep_Inland 7: Continental 8: Deep_Ocean
+    def read_data(self):
+        """
+        reads the data from a VNP03MOD granule into the self.data dictionary.
+        Two variables are read:
+
+        a) land_water_mask
+        1: Shallow_Ocean 2: Land 3: Coastline 4: Shallow_Inland 5: Ephemeral 6: Deep_Inland 7: Continental 8: Deep_Ocean
+        b) quality_flag
+        1: Input_invalid 2: Pointing_bad 3: Terrain_bad
+        :return: None
+        """
+
         self.data['land_water_mask'] = self.netcdf.groups['geolocation_data']['land_water_mask'][:].data
-        
-        # 1: Input_invalid 2: Pointing_bad 3: Terrain_bad
         self.data['quality_flag'] = self.netcdf.groups['geolocation_data']['quality_flag'][:].data
     
 
-class VNP03DNB(VIIRS_L2):
+class VNP03DNB(VIIRSL2):
     
     def __init__(self, file_path, sidecar_path=None):
-        super().__init__(file_path,sidecar_path)        
+        super().__init__(file_path, sidecar_path)
     
-    def read_data(self):        
-        self.data['moon_illumination_fraction'] = self.netcdf.groups['geolocation_data']['moon_illumination_fraction'][:].data
-        
-        # 1: Shallow_Ocean 2: Land 3: Coastline 4: Shallow_Inland 5: Ephemeral 6: Deep_Inland 7: Continental 8: Deep_Ocean
-        self.data['land_water_mask'] = self.netcdf.groups['geolocation_data']['land_water_mask'][:].data
-        
-        # 1: Input_invalid 2: Pointing_bad 3: Terrain_bad
-        self.data['quality_flag'] = self.netcdf.groups['geolocation_data']['quality_flag'][:].data
+    def read_data(self):
+        """
+        reads the data from a VNP03DNB granule into the self.data dictionary.
+        Three variables are read:
+
+        a) moon_illumination_fraction
+        b) land_water_mask
+        1: Shallow_Ocean 2: Land 3: Coastline 4: Shallow_Inland 5: Ephemeral 6: Deep_Inland 7: Continental 8: Deep_Ocean
+        c) quality_flag
+        1: Input_invalid 2: Pointing_bad 3: Terrain_bad
+        :return:
+        """
+
+        group = self.netcdf.groups['geolocation_data']
+        self.data['moon_illumination_fraction'] = group['moon_illumination_fraction'][:].data
+        self.data['land_water_mask'] = group['land_water_mask'][:].data
+        self.data['quality_flag'] = group['quality_flag'][:].data
                 
 
-class VNP02DNB(VIIRS_L2):
+class VNP02DNB(VIIRSL2):
     
     def __init__(self, file_path, sidecar_path=None):
         super(VNP02DNB, self).__init__(file_path, sidecar_path)
@@ -90,4 +105,3 @@ class VNP02DNB(VIIRS_L2):
         
     def read_sidecar_index(self, sidecar_path=None):
         pass
-        
