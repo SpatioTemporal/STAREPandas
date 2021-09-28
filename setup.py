86682ab7a086ab530f2af471d65fd15f4b67bca3
griessbaum <griessbaum@gmail.com>
2021-09-27 19:25:51 -0700

    pypi

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ version = versioneer.get_version(),
 cmdclass = versioneer.get_cmdclass()
 
 setuptools.setup(
-    version=version,
-    #version="0.5.1",
+    version=version,    
     cmdclass=cmdclass
 )
 
