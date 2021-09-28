1a98b1a9d68802c0f6cba70b66392cc7064be5ec
griessbaum <griessbaum@gmail.com>
2021-09-27 20:16:29 -0700

    mv setup

--- setup.py
+++ setup.py
@@ -7,8 +7,11 @@ import versioneer
 version = versioneer.get_version(),
 cmdclass = versioneer.get_cmdclass()
 
+
+
 setuptools.setup(
-    version=version,    
+    version=version[0],   
+    #version='0.6.1',
     cmdclass=cmdclass
 )
 
