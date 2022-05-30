f2e4d1d7ba45a1a66bfbf5cd5b0c08f93fe3612a
niklas <griessbaum@gmail.com>
2022-05-30 13:42:26 -0700

    added pod example

--- starepandas/io/pod.py
+++ starepandas/io/pod.py
@@ -8,18 +8,32 @@ import pandas
 def read_pods(pod_root, sids, pattern, add_podname=False):
     """ Reads a STAREDataframe from a directory of STAREPods
 
-    :param add_podname: toggle if the podname should be read as a parameter
-    :type add_podname: bool
-    :param pod_root: Root directory containing the pods
-    :type pod_root: string
-    :param sids: STARE index values to read pods for
-    :type sids: list/array-like
-    :param pattern: name pattern of chunks to read
-    :type pattern: str
-    :return: starepandas.STAREDataFrame
-    :rtype: starepandas.STAREDataFrame
-    """
+    Parameters
+    -----------
+    pod_root: str
+        Root directory containing the pods
+    sids: array-like
+        STARE index values to read pods for
+    pattern: str
+        name pattern of chunks to read
+    add_podname: bool
+        toggle if the podname should be read as a parameter
 
+    Returns
+    --------
+    df: starepandas.STAREDataFrame
+        A dataframe containing the data of the read pods
+
+    Examples:
+    ----------
+    >>> import starepandas
+    >>> sdf = starepandas.read_pods(pod_root='tests/data/pods/',
+    ...                             sids=['0x0a00000000000004'],
+    ...                             pattern='SSMIS.XCAL2016',
+    ...                             add_podname=True)
+    >>>
+    """
+    dfs = []
     for sid in sids:
         pod_path = '{pod_root}/{sid}/'.format(pod_root=pod_root, sid=sid)
         if not os.path.exists(pod_path):
@@ -28,14 +42,13 @@ def read_pods(pod_root, sids, pattern, add_podname=False):
         pickles = sorted(glob.glob(os.path.expanduser(pod_path + '/*')))
         search = '.*{pattern}.*'.format(pattern=pattern)
         pods = list(filter(re.compile(search).match, pickles))
-        dfs = []
         for pod in pods:
             df = pandas.read_pickle(pod)
             if add_podname:
                 df['pod'] = pod
             dfs.append(df)
     df = pandas.concat(dfs)
-    df.reset_index(inplace=True)
+    df.reset_index(inplace=True, drop=True)
     df = starepandas.STAREDataFrame(df)
     return df
 
