e2590fa4109379618c7e9d6f8ffb68d111877483
niklas <griessbaum@gmail.com>
2022-08-05 10:09:35 -0700

    fixed tests

--- tests/test_na.py
+++ tests/test_na.py
@@ -2,6 +2,6 @@ import starepandas
 
 
 def test():
-    file_path = 'tests/data/MOD09.A2002299.0710.006.2015151173939.hdf'
+    file_path = 'tests/data/granules/MOD09.A2002299.0710.006.2015151173939.hdf'
     sdf = starepandas.read_granule(file_path, sidecar=True)
     sdf.spatial_resolution()
