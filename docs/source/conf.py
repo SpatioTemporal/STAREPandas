66c6d4f2c6b0f8e4656de6811dca50a451f71bcd
Niklas Griessbaum <griessbaum@gmail.com>
2022-09-15 16:59:48 -0700

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -79,6 +79,7 @@ html_theme_options = {
     "github_url": "https://github.com/SpatioTemporal/STAREPandas",
 }
 
+
 autosummary_generate = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -86,11 +87,8 @@ autosummary_generate = True
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-#html_css_files = ['style.css']
+html_css_files = ['style.css']
 #html_css_files = ['https://cdn.jupyter.org/notebook/5.1.0/style/style.min.css']
-
-
-#html_sidebars = {"**": []}
 
 nbsphinx_execute = "auto"
 nbsphinx_allow_errors = True
