3287417c578389252ed7b9bd559c3f6643617e49
Niklas Griessbaum <griessbaum@gmail.com>
2022-09-14 22:04:58 -0700

    test update

--- tests/test_geotiff.py
+++ tests/test_geotiff.py
@@ -4,4 +4,4 @@ import starepandas
 def test_geotiff():
     file_path = 'tests/data/wind.tif'
     sdf = starepandas.read_geotiff(file_path, add_pts=True, add_latlon=True, add_xy=True, add_trixels=True)
-    assert sdf.shape == (93, 8)
+    assert sdf.shape == (93, 6)
