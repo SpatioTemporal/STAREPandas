f3b716a93ed45457c1cb1f4770c17c89127d7a46
niklas <griessbaum@gmail.com>
2023-05-12 14:14:08 -0700

    fixed

--- starepandas/io/granules/atms.py
+++ starepandas/io/granules/atms.py
@@ -3,17 +3,17 @@ import numpy
 
 
 class ATMS(SSMIS):
-    
+
     def read_timestamps(self):
         self.timestamps = {}
-        
+
         for scan in self.scans:
             ts = self.read_timestamp_scan(scan)
             ts_len = ts.shape[0]
             ts = numpy.repeat(ts, 96)
             ts = ts.reshape(ts_len, 96)
             self.timestamps[scan] = ts
-    
+
     def read_data(self):
         if 'S1' in self.scans:
             self.data['S1']['Tc1'] = self.netcdf.groups['S1']['Tc'][:, :, 0]
@@ -31,4 +31,3 @@ class ATMS(SSMIS):
             self.data['S4']['Tc4'] = self.netcdf.groups['S4']['Tc'][:, :, 3]
             self.data['S4']['Tc5'] = self.netcdf.groups['S4']['Tc'][:, :, 4]
             self.data['S4']['Tc6'] = self.netcdf.groups['S4']['Tc'][:, :, 5]
-
