d08c381ab96cba5f2bd2b6117caf020beb5c6ba8
Dai-Hai Ton-That <tonhai@Dai-Hais-MacBook-Air.local>
2025-06-22 18:36:04 -0700

    Add HDF5 package. Add tests for ssmis. Add and example file for SSMIS HDF5

--- starepandas/io/granules/ssmis.py
+++ starepandas/io/granules/ssmis.py
@@ -3,13 +3,13 @@ import starepandas.io.s3
 import datetime
 import numpy
 import pystare
+import os
 
 
 class SSMIS(Granule):
-    
+
     def __init__(self, file_path, sidecar_path=None, scans=['S1', 'S2', 'S3', 'S4']):
         super().__init__(file_path, sidecar_path)
-        self.netcdf = starepandas.io.s3.nc4_dataset_wrapper(self.file_path, 'r', format='NETCDF4')
         self.lat = None
         self.lon = None
         self.timestamps = {}
@@ -18,32 +18,171 @@ class SSMIS(Granule):
         self.nom_res = ''
         self.scans = scans
         self.header = {}
+        self.file_type = self._determine_file_type()
+        self.dataset = self._open_dataset()
         self.parse_header()
+
+    def _determine_file_type(self):
+        """Determine if the file is NetCDF4 or HDF5 based on file extension or content."""
+        if self.file_path.lower().endswith('.h5') or self.file_path.lower().endswith('.hdf5'):
+            return 'hdf5'
+        elif self.file_path.lower().endswith('.nc') or self.file_path.lower().endswith('.nc4'):
+            return 'netcdf4'
+        else:
+            # Try to determine by attempting to open as HDF5 first, then NetCDF4
+            try:
+                if 's3://' == self.file_path[0:5]:
+                    s3 = starepandas.io.s3.parse_s3_url(self.file_path)
+                    try:
+                        import boto3
+                        s3_client = boto3.client('s3')
+                        test_file = starepandas.io.s3.h5_dataset_from_s3(
+                            s3_client, s3['bucket_name'],
+                            s3['prefix'] + s3['prefix_end'] + s3['resource']
+                        )
+                        test_file.close()
+                        return 'hdf5'
+                    except ImportError:
+                        raise ImportError("boto3 is required for S3 HDF5 file access")
+                else:
+                    try:
+                        import h5py
+                        with h5py.File(self.file_path, 'r') as test_file:
+                            pass
+                        return 'hdf5'
+                    except ImportError:
+                        raise ImportError("h5py is required for HDF5 file access")
+            except Exception:
+                try:
+                    if 's3://' == self.file_path[0:5]:
+                        s3 = starepandas.io.s3.parse_s3_url(self.file_path)
+                        try:
+                            import boto3
+                            s3_client = boto3.client('s3')
+                            test_file = starepandas.io.s3.nc4_dataset_from_s3(
+                                s3_client, s3['bucket_name'],
+                                s3['prefix'] + s3['prefix_end'] + s3['resource']
+                            )
+                            test_file.close()
+                            return 'netcdf4'
+                        except ImportError:
+                            raise ImportError("boto3 is required for S3 NetCDF4 file access")
+                    else:
+                        try:
+                            import netCDF4
+                            with netCDF4.Dataset(self.file_path, 'r') as test_file:
+                                pass
+                            return 'netcdf4'
+                        except ImportError:
+                            raise ImportError("netCDF4 is required for NetCDF4 file access")
+                except Exception:
+                    # Default to NetCDF4 if we can't determine
+                    return 'netcdf4'
+
+    def _open_dataset(self):
+        """Open the dataset based on file type."""
+        if self.file_type == 'hdf5':
+            if 's3://' == self.file_path[0:5]:
+                s3 = starepandas.io.s3.parse_s3_url(self.file_path)
+                try:
+                    import boto3
+                    s3_client = boto3.client('s3')
+                    return starepandas.io.s3.h5_dataset_from_s3(
+                        s3_client, s3['bucket_name'],
+                        s3['prefix'] + s3['prefix_end'] + s3['resource']
+                    )
+                except ImportError:
+                    raise ImportError("boto3 is required for S3 HDF5 file access")
+            else:
+                try:
+                    import h5py
+                    return h5py.File(self.file_path, 'r')
+                except ImportError:
+                    raise ImportError("h5py is required for HDF5 file access")
+        else:  # netcdf4
+            return starepandas.io.s3.nc4_dataset_wrapper(self.file_path, 'r', format='NETCDF4')
 
     def parse_header(self):
         self.header = {}
-        for h in self.netcdf.FileHeader.replace(';', '').strip().split('\n'):
-            key = h.split('=')[0]
-            value = h.split('=')[1]
-            self.header[key] = value
-        self.ts_start = self.header['StartGranuleDateTime']
-        self.ts_end = self.header['StopGranuleDateTime']
+        if self.file_type == 'hdf5':
+            # For HDF5, try to get header from attributes
+            if 'FileHeader' in self.dataset.attrs:
+                header_str = self.dataset.attrs['FileHeader']
+                # Handle bytes vs string conversion for HDF5
+                if isinstance(header_str, bytes):
+                    header_str = header_str.decode('utf-8')
+                elif isinstance(header_str, numpy.ndarray):
+                    header_str = str(header_str)
+            else:
+                # Try to find header in global attributes
+                header_str = ""
+                for key, value in self.dataset.attrs.items():
+                    if 'header' in key.lower() or 'metadata' in key.lower():
+                        if isinstance(value, bytes):
+                            header_str = value.decode('utf-8')
+                        else:
+                            header_str = str(value)
+                        break
+
+                if not header_str:
+                    # Create minimal header from available attributes
+                    self.header = {}
+                    for key, value in self.dataset.attrs.items():
+                        if isinstance(value, bytes):
+                            self.header[key] = value.decode('utf-8')
+                        else:
+                            self.header[key] = str(value)
+                    self.ts_start = self.header.get('StartGranuleDateTime', '')
+                    self.ts_end = self.header.get('StopGranuleDateTime', '')
+                    return
+        else:  # netcdf4
+            try:
+                header_str = self.dataset.FileHeader
+            except AttributeError:
+                # FileHeader attribute not found, create minimal header
+                self.header = {}
+                self.ts_start = ''
+                self.ts_end = ''
+                return
+
+        # Parse header string
+        if header_str:
+            for h in header_str.replace(';', '').strip().split('\n'):
+                if '=' in h:
+                    key = h.split('=')[0]
+                    value = h.split('=')[1]
+                    self.header[key] = value
+        self.ts_start = self.header.get('StartGranuleDateTime', '')
+        self.ts_end = self.header.get('StopGranuleDateTime', '')
 
     def read_latlon(self):
         self.lat = {}
         self.lon = {}
         for scan in self.scans:
-            self.lat[scan] = self.netcdf.groups[scan]['Latitude'][:].astype(numpy.double)
-            self.lon[scan] = self.netcdf.groups[scan]['Longitude'][:].astype(numpy.double)
+            if self.file_type == 'hdf5':
+                self.lat[scan] = self.dataset[scan]['Latitude'][:].astype(numpy.double)
+                self.lon[scan] = self.dataset[scan]['Longitude'][:].astype(numpy.double)
+            else:  # netcdf4
+                self.lat[scan] = self.dataset.groups[scan]['Latitude'][:].astype(numpy.double)
+                self.lon[scan] = self.dataset.groups[scan]['Longitude'][:].astype(numpy.double)
 
     def read_timestamp_scan(self, scan):
-        year = self.netcdf.groups[scan]['ScanTime']['Year'][:]
-        month = self.netcdf.groups[scan]['ScanTime']['Month'][:]
-        day = self.netcdf.groups[scan]['ScanTime']['DayOfMonth'][:]
-        hour = self.netcdf.groups[scan]['ScanTime']['Hour'][:]
-        minute = self.netcdf.groups[scan]['ScanTime']['Minute'][:]
-        second = self.netcdf.groups[scan]['ScanTime']['Second'][:]
-        millisecond = self.netcdf.groups[scan]['ScanTime']['MilliSecond'][:]
+        if self.file_type == 'hdf5':
+            year = self.dataset[scan]['ScanTime']['Year'][:]
+            month = self.dataset[scan]['ScanTime']['Month'][:]
+            day = self.dataset[scan]['ScanTime']['DayOfMonth'][:]
+            hour = self.dataset[scan]['ScanTime']['Hour'][:]
+            minute = self.dataset[scan]['ScanTime']['Minute'][:]
+            second = self.dataset[scan]['ScanTime']['Second'][:]
+            millisecond = self.dataset[scan]['ScanTime']['MilliSecond'][:]
+        else:  # netcdf4
+            year = self.dataset.groups[scan]['ScanTime']['Year'][:]
+            month = self.dataset.groups[scan]['ScanTime']['Month'][:]
+            day = self.dataset.groups[scan]['ScanTime']['DayOfMonth'][:]
+            hour = self.dataset.groups[scan]['ScanTime']['Hour'][:]
+            minute = self.dataset.groups[scan]['ScanTime']['Minute'][:]
+            second = self.dataset.groups[scan]['ScanTime']['Second'][:]
+            millisecond = self.dataset.groups[scan]['ScanTime']['MilliSecond'][:]
 
         timestamps = []
         for d in zip(year, month, day, hour, minute, second, millisecond):
@@ -55,7 +194,7 @@ class SSMIS(Granule):
             else:
                 timestamps.append(datetime.datetime(*d))
         return numpy.array(timestamps)
-            
+
     def read_timestamps(self):
         if 'S1' in self.scans:
             ts = self.read_timestamp_scan('S1')
@@ -87,25 +226,47 @@ class SSMIS(Granule):
 
     def read_data(self):
         if 'S1' in self.scans:
-            self.data['S1']['Tc1'] = self.netcdf.groups['S1']['Tc'][:, :, 0]
-            self.data['S1']['Tc2'] = self.netcdf.groups['S1']['Tc'][:, :, 1]
-            self.data['S1']['Tc3'] = self.netcdf.groups['S1']['Tc'][:, :, 2]
+            if self.file_type == 'hdf5':
+                self.data['S1']['Tc1'] = self.dataset['S1']['Tc'][:, :, 0]
+                self.data['S1']['Tc2'] = self.dataset['S1']['Tc'][:, :, 1]
+                self.data['S1']['Tc3'] = self.dataset['S1']['Tc'][:, :, 2]
+            else:  # netcdf4
+                self.data['S1']['Tc1'] = self.dataset.groups['S1']['Tc'][:, :, 0]
+                self.data['S1']['Tc2'] = self.dataset.groups['S1']['Tc'][:, :, 1]
+                self.data['S1']['Tc3'] = self.dataset.groups['S1']['Tc'][:, :, 2]
 
         if 'S2' in self.scans:
-            self.data['S2']['Tc1'] = self.netcdf.groups['S2']['Tc'][:, :, 0]
-            self.data['S2']['Tc2'] = self.netcdf.groups['S2']['Tc'][:, :, 1]
+            if self.file_type == 'hdf5':
+                self.data['S2']['Tc1'] = self.dataset['S2']['Tc'][:, :, 0]
+                self.data['S2']['Tc2'] = self.dataset['S2']['Tc'][:, :, 1]
+            else:  # netcdf4
+                self.data['S2']['Tc1'] = self.dataset.groups['S2']['Tc'][:, :, 0]
+                self.data['S2']['Tc2'] = self.dataset.groups['S2']['Tc'][:, :, 1]
 
         if 'S3' in self.scans:
-            self.data['S3']['Tc1'] = self.netcdf.groups['S3']['Tc'][:, :, 0]
-            self.data['S3']['Tc2'] = self.netcdf.groups['S3']['Tc'][:, :, 1]
-            self.data['S3']['Tc3'] = self.netcdf.groups['S3']['Tc'][:, :, 2]
-            self.data['S3']['Tc4'] = self.netcdf.groups['S3']['Tc'][:, :, 3]
+            if self.file_type == 'hdf5':
+                self.data['S3']['Tc1'] = self.dataset['S3']['Tc'][:, :, 0]
+                self.data['S3']['Tc2'] = self.dataset['S3']['Tc'][:, :, 1]
+                self.data['S3']['Tc3'] = self.dataset['S3']['Tc'][:, :, 2]
+                self.data['S3']['Tc4'] = self.dataset['S3']['Tc'][:, :, 3]
+            else:  # netcdf4
+                self.data['S3']['Tc1'] = self.dataset.groups['S3']['Tc'][:, :, 0]
+                self.data['S3']['Tc2'] = self.dataset.groups['S3']['Tc'][:, :, 1]
+                self.data['S3']['Tc3'] = self.dataset.groups['S3']['Tc'][:, :, 2]
+                self.data['S3']['Tc4'] = self.dataset.groups['S3']['Tc'][:, :, 3]
 
         if 'S4' in self.scans:
-            self.data['S4']['Tc1'] = self.netcdf.groups['S4']['Tc'][:, :, 0]
-            self.data['S4']['Tc2'] = self.netcdf.groups['S4']['Tc'][:, :, 1]
+            if self.file_type == 'hdf5':
+                self.data['S4']['Tc1'] = self.dataset['S4']['Tc'][:, :, 0]
+                self.data['S4']['Tc2'] = self.dataset['S4']['Tc'][:, :, 1]
+            else:  # netcdf4
+                self.data['S4']['Tc1'] = self.dataset.groups['S4']['Tc'][:, :, 0]
+                self.data['S4']['Tc2'] = self.dataset.groups['S4']['Tc'][:, :, 1]
 
     def add_sids(self, adapt_resolution=True):
+        if self.lat is None:
+            raise ValueError("Latitude and longitude data must be loaded before adding SIDs. Call read_latlon() first.")
+
         self.sids = {}
         for scan in self.scans:
             self.sids[scan] = pystare.from_latlon2D(lat=self.lat[scan], lon=self.lon[scan],
@@ -164,5 +325,16 @@ class SSMIS(Granule):
             df = starepandas.STAREDataFrame(df)
             dfs[scan] = df
         return dfs
+
+    def close(self):
+        """Close the dataset file."""
+        if hasattr(self, 'dataset') and self.dataset is not None:
+            self.dataset.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
 
 
